a41299ca4bd26a8be15c1647a79b348cf73458d2
Hohman <0308288@students.dadeschools.net>
2019-04-16 10:22:27 -0400

    finished html and started js

---
+++ main.js
@@ -0,0 +1,28 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+
+// Question set
+ var questions = [
+
+    {
+        question: "What was the first full length CGI movie?",
+        answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+        correctAnswer: "Toy Story",
+        image:"assets\toyStory.jpg"
+    },
+
+    {
+        question: "In what year did the movie Sandlot come out?",
+        answers: ["1991", "1999", "1997", "1993"],
+        correctAnswer: "1993",
+        image:"assets\"sandlot.jpg"
+    },
+
+
+
+
+
+]
+
